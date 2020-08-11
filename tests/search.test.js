264d4202088627dde2c1aaec10268b35b0170a0a
Payton Swick <payton@foolord.com>
2020-08-11 19:49:46 -0400

    Expand search test for all js examples

--- tests/search.test.js
+++ tests/search.test.js
@@ -1,21 +1,25 @@
 // @ts-check
 const { search } = require('../src/general');
 
-describe('search', () => {
-	it('works', async () => {
+describe.each([
+	['queryDb', 1],
+	['makeQuery', 3],
+	['parseQuery', 5],
+	['objectWithFunctionShorthand', 9],
+	['shorthandFunction', 10],
+])("search('%s')", (symbol, expectedLine) => {
+	test(`finds line '${expectedLine}'`, async () => {
+		const path = './tests/fixtures/js/db.js';
 		const config = {
 			type: 'js',
 			searchTool: 'ripgrep',
-			path: './tests/fixtures/js/db.js',
+			path,
 		};
-		const expected = [
-			{
-				path: './tests/fixtures/js/db.js',
-				line: 3,
-				text: 'const makeQuery = () => {};',
-			},
-		];
-		const result = await search('makeQuery', config);
-		expect(result).toEqual(expected);
+		const results = await search(symbol, config);
+		expect(results.length).toEqual(1);
+		const result = results[0];
+		expect(result.path).toEqual(path);
+		expect(result.line).toEqual(expectedLine);
+		expect(result.text).toBeDefined();
 	});
 });
