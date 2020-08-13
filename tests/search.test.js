4e61df1fe7df122a258d24920ee258b1f1d9fe03
Payton Swick <payton@foolord.com>
2020-08-13 18:37:47 -0400

    Update tests to support multiple files in path

--- tests/search.test.js
+++ tests/search.test.js
@@ -6,12 +6,12 @@ describe.each([
 	['makeQuery', 'js', 'js', 3],
 	['parseQuery', 'js', 'js', 5],
 	['parseQuery', 'js', 'js_2_files', 5],
-	['parseQuery', 'js', 'js_glob_files', 5],
+	['parseQuery', 'js', 'js_directory', 5],
 	['objectWithFunctionShorthand', 'js', 'js', 9],
 	['shorthandFunction', 'js', 'js', 10],
 	['shorthandFunction', undefined, 'js', 10], // auto-detect type
 	['shorthandFunction', undefined, 'js_2_files', 10], // auto_detect type
-	['shorthandFunction', undefined, 'js_glob_files', 10], // auto_detect type
+	['shorthandFunction', undefined, 'js_directory', 10], // auto_detect type
 	['queryDb', 'php', 'php', 2],
 	['$makeQuery', 'php', 'php', 4],
 	['parseQuery', 'php', 'php', 6],
@@ -30,7 +30,7 @@ describe.each([
 		const results = await search(symbol, config);
 		expect(results.length).toEqual(1);
 		const result = results[0];
-		expect(result.path).toEqual(path);
+		expect(result.path).toEqual(getExpectedResultPath(fixtureType));
 		expect(result.line).toEqual(expectedLine);
 		expect(result.text).toBeDefined();
 	});
@@ -46,8 +46,25 @@ function getFixtureForType(type) {
 			return './tests/fixtures/js/db.js';
 		case 'js_2_files':
 			return './tests/fixtures/js/db.js ./tests/fixtures/js/other.js';
-		case 'js_glob_files':
-			return './tests/fixtures/js/*';
+		case 'js_directory':
+			return './tests/fixtures/js';
+		case 'php':
+			return './tests/fixtures/php/db.php';
+		default:
+			throw new Error(`Could not find fixture for type '${type}'`);
+	}
+}
+
+/**
+ * @param {string} type
+ * @returns {string} path
+ */
+function getExpectedResultPath(type) {
+	switch (type) {
+		case 'js':
+		case 'js_2_files':
+		case 'js_directory':
+			return './tests/fixtures/js/db.js';
 		case 'php':
 			return './tests/fixtures/php/db.php';
 		default:
