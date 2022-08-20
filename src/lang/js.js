75f17d249cc12d7b77bbda2dc7526a716295f2ee
Payton Swick <payton@foolord.com>
2022-08-20 17:10:03 -0400

    Remove regexps for methodLonghand
    propertyLonghand cover these.

--- src/lang/js.js
+++ src/lang/js.js
@@ -14,15 +14,11 @@ function getRegexp(symbol) {
 	const symbolWithDeclaration = `(${declarationKeywords.map(addTrailingSpace).join('|')})${symbol}\\b`;
 	const prototypeDeclaration = `prototype\\.${symbol}\\b`
 	const methodShorthand = `${symbol}\\([^)]*\\)\\s*(:[^{]+)?\\{`;
-	const methodLonghand = `${symbol}:\\s*function\\([^)]*\\)\\s*\\{`;
-	const methodArrowFunction = `${symbol}:\\s*\\([^)]*\\)\\s*=>\\s*\\{`;
 	const propertyLonghand = `${symbol}:\\s*`;
 	const regexpParts = [
 		symbolWithDeclaration,
 		prototypeDeclaration,
 		methodShorthand,
-		methodLonghand,
-		methodArrowFunction,
 		propertyLonghand,
 	];
 	return `\\b(${[regexpParts.join('|')]})`;
