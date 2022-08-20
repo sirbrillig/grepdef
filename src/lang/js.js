614fc5c824b26493890d87c047ccaee67df132a9
Payton Swick <payton@foolord.com>
2022-08-20 16:28:28 -0400

    Support searching method longhand

--- src/lang/js.js
+++ src/lang/js.js
@@ -14,10 +14,14 @@ function getRegexp(symbol) {
 	const symbolWithDeclaration = `(${declarationKeywords.map(addTrailingSpace).join('|')})${symbol}\\b`;
 	const prototypeDeclaration = `prototype\\.${symbol}\\b`
 	const methodShorthand = `${symbol}\\([^)]*\\)\\s*\\{`;
+	const methodLonghand = `${symbol}:\\s*function\\([^)]*\\)\\s*\\{`;
+	const methodArrowFunction = `${symbol}:\\s*\\([^)]*\\)\\s*=>\\s*\\{`;
 	const regexpParts = [
 		symbolWithDeclaration,
 		prototypeDeclaration,
 		methodShorthand,
+		methodLonghand,
+		methodArrowFunction,
 	];
 	return `\\b(${[regexpParts.join('|')]})`;
 }
